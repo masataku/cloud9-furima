806f288a7f24e4fe31ba55872962d26f32e1e273
EC2 Default User <ec2-user@ip-172-31-12-47.ap-northeast-1.compute.internal>
2020-06-18 06:24:38 +0000

    create show

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200618054424) do
+ActiveRecord::Schema.define(version: 20200618060953) do
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200618054424) do
     t.integer  "point"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "password"
   end
 
 end
