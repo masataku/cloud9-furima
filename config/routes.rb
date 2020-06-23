fd5deba4f01f50cecb0f7bab11598c42f15533fe
EC2 Default User <ec2-user@ip-172-31-12-47.ap-northeast-1.compute.internal>
2020-06-23 11:24:49 +0000

    OK

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root 'top#index'
   get 'users/login' => 'users#login_form'
   post 'users/login' => 'users#login'
-  get 'users/logout' => 'users#logout' #今のところはpathをgetにしているが、session付けたら変更
+  post 'users/logout' => 'users#logout' #今のところはpathをgetにしているが、session付けたら変更
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
