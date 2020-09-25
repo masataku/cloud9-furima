209715f8cc980a1f246014e3bc6e793f0773f044
EC2 Default User <ec2-user@ip-172-31-12-47.ap-northeast-1.compute.internal>
2020-09-25 07:49:55 +0000

    create alert detail

--- app/controllers/top_controller.rb
+++ app/controllers/top_controller.rb
@@ -1,5 +1,8 @@
 class TopController < ApplicationController
   def index
     @items = Item.limit(12).order(created_at: :desc)
+    if @current_user
+      @like_items = @current_user.like_items.limit(5).order(created_at: :desc)
+    end
   end
 end
--- app/models/like.rb
+++ app/models/like.rb
@@ -9,6 +9,7 @@
 #  updated_at :datetime         not null
 #
 class Like < ApplicationRecord
+  default_scope->{order(created_at: :desc)}
   belongs_to :user
   belongs_to :item
   
